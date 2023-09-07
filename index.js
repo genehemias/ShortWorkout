3417455719d3c6da82d5cba17c3d226caba04622
Gabriel Nehemias <genehemias@gmail.com>
2023-09-07 14:50:40 -0400

    adds rest timer

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const btnStart = document.getElementById("btn-start");
 
 let started = false;
-let exerciseTimeUnit = 30;
-// let restTimeUnit = 10;
-// let numberOfExercises = 6;
+let exerciseTimeUnit = 6;
+let restTimeUnit = 3;
+let numberOfExercises = 6;
+let resting = false;
+let exercisesDone = 0;
 let exerciseTimer = new easytimer.Timer();
-// let restTimer = new easyTimer.Timer();
+let restTimer = new easytimer.Timer();
 let progressBar = document.getElementById("progress-bar");
 let root = document.documentElement;
 
@@ -14,27 +16,77 @@ exerciseTimer.addEventListener('secondsUpdated', function (e) {
     timeLeft = timeLeft.substr(6,2);
     console.log(timeLeft);
     timeLeftRatio = (timeLeft / exerciseTimeUnit) * 100;
-    console.log(timeLeftRatio);
+    //console.log(timeLeftRatio);
     root.style.setProperty("--progress-value", timeLeftRatio + "%");
 });
 
 exerciseTimer.addEventListener('targetAchieved', function (e) {
-    console.log("TIMER ENDED");
+    console.log("WORK TIMER ENDED");
     exerciseTimer.reset();
     exerciseTimer.pause();
-    btnStart.innerText = "Start";
-    started = !started;
+
+    exercisesDone++;
+    
+    console.log(`resting: ${resting}`)
+    
+    if (exercisesDone >= numberOfExercises) {
+        //all done! reset UI
+        btnStart.innerText = "Start";
+        started = !started;
+    } else {
+        startRestTimer();
+    }
+
+    resting = true;
+});
+
+restTimer.addEventListener('targetAchieved', function (e) {
+    console.log("REST TIMER ENDED");
+    restTimer.reset();
+    restTimer.pause();
+    startExerciseTimer();
+    resting = false;
+})
+
+restTimer.addEventListener('secondsUpdated', function (e) {
+    let timeLeft = restTimer.getTimeValues().toString();
+    timeLeft = timeLeft.substr(6,2);
+    console.log(timeLeft);
+    timeLeftRatio = (timeLeft / restTimeUnit) * 100;
+    root.style.setProperty("--progress-value", timeLeftRatio + "%");
 });
 
 function btnStartClick() {
-    if (!started) {        
-        root.style.setProperty("--progress-value", "100%");
-        exerciseTimer.start({countdown:true, startValues:{seconds:exerciseTimeUnit}});
+    if (!started) {
+        if (!resting)
+        { startExerciseTimer(); }
+        else
+        { startRestTimer(); }
         btnStart.innerText = "Pause";
     } else {
         btnStart.innerText = "Start";
         exerciseTimer.pause();
+        restTimer.pause();
     }
     started = !started;
 }
 
+function startRestTimer() {
+    console.log("starting REST timer");
+    if (restTimer.isPaused()) {
+        restTimer.start();
+    } else {
+        root.style.setProperty("--progress-value", "100%");
+        restTimer.start({countdown:true, startValues:{seconds:restTimeUnit + 1}});    
+    }
+}
+
+function startExerciseTimer() {
+    console.log("starting WORK timer");
+    if (exerciseTimer.isPaused()) {
+        exerciseTimer.start();
+    } else {
+        root.style.setProperty("--progress-value", "100%");
+        exerciseTimer.start({countdown:true, startValues:{seconds:exerciseTimeUnit + 1}});    
+    }    
+}
