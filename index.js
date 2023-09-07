56a9ac8b567ac2116111d24093ae7bf429b6ee27
Gabriel Nehemias <genehemias@gmail.com>
2023-09-07 14:19:52 -0400

    enhance timer end behavior

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const btnStart = document.getElementById("btn-start");
 
 let started = false;
+let exerciseTimeUnit = 30;
+// let restTimeUnit = 10;
+// let numberOfExercises = 6;
 let exerciseTimer = new easytimer.Timer();
+// let restTimer = new easyTimer.Timer();
 let progressBar = document.getElementById("progress-bar");
 let root = document.documentElement;
 
@@ -9,20 +13,23 @@ exerciseTimer.addEventListener('secondsUpdated', function (e) {
     let timeLeft = exerciseTimer.getTimeValues().toString();
     timeLeft = timeLeft.substr(6,2);
     console.log(timeLeft);
-    timeLeftRatio = (timeLeft / 30) * 100;
+    timeLeftRatio = (timeLeft / exerciseTimeUnit) * 100;
     console.log(timeLeftRatio);
     root.style.setProperty("--progress-value", timeLeftRatio + "%");
-    progressBar.ariaValueNow = timeLeftRatio;
 });
 
 exerciseTimer.addEventListener('targetAchieved', function (e) {
     console.log("TIMER ENDED");
-    //exerciseTimer.reset();
+    exerciseTimer.reset();
+    exerciseTimer.pause();
+    btnStart.innerText = "Start";
+    started = !started;
 });
 
 function btnStartClick() {
-    if (!started) {
-        exerciseTimer.start({countdown:true, startValues:{seconds:30}});
+    if (!started) {        
+        root.style.setProperty("--progress-value", "100%");
+        exerciseTimer.start({countdown:true, startValues:{seconds:exerciseTimeUnit}});
         btnStart.innerText = "Pause";
     } else {
         btnStart.innerText = "Start";
