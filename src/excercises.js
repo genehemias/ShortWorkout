6073b2c79934e66f1327e3beaf6912e6e44098fd
Gabriel Nehemias <genehemias@gmail.com>
2023-09-11 21:26:56 -0400

    js links to image files

--- src/excercises.js
+++ src/excercises.js
@@ -23,18 +23,18 @@ function Excercise(_id, _name, _description, _weights, _img)
 }
 
 const exercises = [
-    Excercise(1, "Jumping Jacks", "do a jumping jack", false, null),
-    Excercise(2, "Lunges", "step forward & crouch down a bit", false, null),
-    Excercise(3, "Calf Raises", "stand on your tippy toes", false, null),
-    Excercise(4, "Star Jump", "like a jumping jack, but start in a squat", false, null),
-    Excercise(5, "Squat Thrusts", "Start like you're going to do a push up, but kick your legs back instead", false, null),
-    Excercise(6, "Skip Rope", "Use a pretend rope", false, null),
-    Excercise(7, "Bridging", "Lay on your back, knees bent, and elevate your hips", false, null),
-    Excercise(8, "Dumbell Curl", "Palms forward. Elbows in. Both arms.", true, null),
-    Excercise(9, "Wide Pushup", "Like a pushup but your hands are farther apart. lower till your elbows are 90 degrees", false, null ),
-    Excercise(10, "Standing Row", "Palms down. Raise weights to chest height, elbows out.", true, null),
-    Excercise(11, "Reverse Dumbell Curl", "Palms down. Elbows in. Both arms.", true, null),
-    Excercise(12, "Lateral Raise", "Palms inward. Raise both arms into a 'T' pose, elbows straight.", true, null)
+    Excercise(1, "Jumping Jacks", "do a jumping jack", false, "resources/jumping jack.jpg"),
+    Excercise(2, "Lunges", "step forward & crouch down a bit", false, "resources/lunge.jpg"),
+    Excercise(3, "Calf Raises", "stand on your tippy toes", false, "resources/calf raise.jpg"),
+    Excercise(4, "Star Jump", "like a jumping jack, but start in a squat", false, "resources/star jump.jpg"),
+    Excercise(5, "Squat Thrusts", "Start like you're going to do a push up, but kick your legs back instead", false, "resources/squat thrust.jpg"),
+    Excercise(6, "Skip Rope", "Use a pretend rope", false, "resources/skip rope.jpg"),
+    Excercise(7, "Bridging", "Lay on your back, knees bent, and elevate your hips", false, "resources/bridging.jpg"),
+    Excercise(8, "Dumbell Curl", "Palms forward. Elbows in. Both arms.", true, "resources/dumbell curl.jpg"),
+    Excercise(9, "Wide Pushup", "Like a pushup but your hands are farther apart. lower till your elbows are 90 degrees", false, "resources/wide pushup.png"),
+    Excercise(10, "Standing Row", "Palms down. Raise weights to chest height, elbows out.", true, "resources/upright row.jpg"),
+    Excercise(11, "Reverse Dumbell Curl", "Palms down. Elbows in. Both arms.", true, "resources/reverse dumbell curl.jpg"),
+    Excercise(12, "Lateral Raise", "Palms inward. Raise both arms into a 'T' pose, elbows straight.", true, "resources/lateral raise.jpg")
 ];
 
 export {completedExercises, nextExcercise};
